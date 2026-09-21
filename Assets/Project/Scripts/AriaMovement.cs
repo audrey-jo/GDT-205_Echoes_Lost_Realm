f602d46ebf2651713faa828011a8e2205c6c9c86
Audrey <acjseltmann@gmail.com>
2026-09-20 21:45:46 -0700

    test hopefully

--- Assets/Project/Scripts/AriaMovement.cs
+++ Assets/Project/Scripts/AriaMovement.cs
@@ -2,21 +2,71 @@ using UnityEngine;
 
 public class AriaMovement : MonoBehaviour
 {
+    // INPUT
+
     private PlayerInput input;
+
     private Vector2 movementInput;
+
+
+    // MOVEMENT
+
     [SerializeField] private float moveSpeed = 5f;
-    void Awake()
+
+
+    // START
+
+    private void Awake()
     {
+        // Create the input controls
         input = new PlayerInput();
     }
 
-    void OnEnable() => input.Enable();
-    void OnDisable() => input.Disable();
 
-    void Update()
+    private void OnEnable()
     {
-        movementInput = input.Aria.Move.ReadValue<Vector2>();
-        Vector3 move = new Vector3(movementInput.x, movementInput.y, 0) * moveSpeed * Time.deltaTime;
-        transform.Translate(move, Space.World);
+        // Make sure input exists
+        if (input == null)
+        {
+            input = new PlayerInput();
+        }
+
+        // Turn controls on
+        input.Enable();
+    }
+
+
+    private void OnDisable()
+    {
+        // Only disable if input exists
+        if (input != null)
+        {
+            input.Disable();
+        }
+    }
+
+
+    // MOVEMENT
+
+    private void Update()
+    {
+        // Get movement input
+        movementInput =
+            input.Aria.Move.ReadValue<Vector2>();
+
+
+        // Make movement direction
+        Vector3 move = new Vector3(
+            movementInput.x,
+            movementInput.y,
+            0f
+        );
+
+
+        // Move Aria
+        transform.Translate(
+            move * moveSpeed * Time.deltaTime,
+            Space.World
+        );
     }
 }
--- Project/Scripts/Textbook/Chapter3/GameData.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class GameData : MonoBehaviour
-{
-    public bool bGameActive = false;
-}
